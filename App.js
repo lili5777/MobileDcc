76e513672b53af7d80abbe252e13e7cde307c6a9
ookapratama <ookapratama2@gmail.com>
2023-12-20 18:51:23 +0800

    projek Mobile DCC

---
+++ App.js
@@ -0,0 +1,13 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text>hello word</Text>
+      <Text>ferdiansyah</Text>
+    </View>
+  );
+};
+export default App;
+const styles = StyleSheet.create({});
